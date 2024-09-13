45e442adeb2c1caa2cb1468438f60fd2c4ad7d49
tfd-ed <info@tfdevs.com>
2024-09-13 10:03:16 +0700

    D2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Library></Library>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Library from "@/components/Library.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    Library,
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+};
+</script>
---
+++ src/components/Library.vue
@@ -0,0 +1,51 @@
+<template>
+  <Card
+    v-for="(item, index) in books"
+    :key="index"
+    :title="item.title"
+    :description="item.description"
+    :image="item.image"
+  ></Card>
+</template>
+<script>
+import Card from "@/components/Card.vue";
+export default {
+  components: { Card },
+  data() {
+    return {
+      books: [
+        {
+          title: "Le Cambodge",
+          page: 454,
+          author: "Jonh",
+          image: "https://placehold.jp/150x150.png",
+        },
+        {
+          title: "Histroy of Cambodia",
+          page: 454,
+          author: "Jonh",
+          image: "https://placehold.jp/150x150.png",
+        },
+        {
+          title: "Le Cambodge sdsf",
+          page: 454,
+          author: "Jonh",
+          image: "https://placehold.jp/150x150.png",
+        },
+        {
+          title: "Le Cambodge 242",
+          page: 454,
+          author: "Jonh",
+          image: "https://placehold.jp/150x150.png",
+        },
+        {
+          title: "Le Cambodge",
+          page: 454,
+          author: "Jonh",
+          image: "https://placehold.jp/150x150.png",
+        },
+      ],
+    };
+  },
+};
+</script>
